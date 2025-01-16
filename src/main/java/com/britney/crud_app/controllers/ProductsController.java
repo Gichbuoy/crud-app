baf8d5eba2cd35215983aeacf16664fb0f8403d7
Alex Maina <lexizgichbuoy@gmail.com>
2025-01-16 23:56:39 +0300

    add controller

--- src/main/java/com/britney/crud_app/controllers/ProductsController.java
+++ src/main/java/com/britney/crud_app/controllers/ProductsController.java
@@ -6,11 +6,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.britney.crud_app.models.Product;
+import com.britney.crud_app.models.ProductDto;
 import com.britney.crud_app.services.ProductsRepository;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("products")
@@ -24,5 +31,27 @@ public class ProductsController {
         List<Product> products = repo.findAll(Sort.by(Sort.Direction.DESC, "id"));
         model.addAttribute("products", products);
         return "products/index";
+    }
+
+    @GetMapping("/create")
+    public String showCreatePage(Model model) {
+        ProductDto productDto = new ProductDto();
+        model.addAttribute("productDto", productDto);
+        return "products/CreateProduct";
+    }
+
+    @PostMapping("create")
+    public String createProduct(
+        @Valid @ModelAttribute ProductDto productDto,
+        BindingResult result
+    ) {
+        if (productDto.getImageFile().isEmpty()) {
+            result.addError(new FieldError("productDto", "imageFile", "The image file is required!"));
+        }
+
+        if (result.hasErrors()) {
+            return "products/CreateProduct";
+        }
+        return "redirect:/products";
     }
 }
